1533b6148964bb39f3106f817d57bd610b72c377
CidJd2 <cidjd2@gmail.com>
2018-03-03 19:29:25 -0500

    Update prometheus.js

--- prometheus.js
+++ prometheus.js
@@ -34,7 +34,7 @@ client.on('message', message => {
 			searchURL,
 			["http://code.jquery.com/jquery.js"],
 			function (err, window) {
-				count = window.$('div table tbody tr').length;
+				count = window.$('tbody').length;
 				resDisplay += searchURL;
 				if (count < numOfResults) {
 					numOfResults = count;
