d0372c2d27a79756184e98b4f49312d571bdc79d
NassimCherrak <37450061+NassimCherrak@users.noreply.github.com>
2018-04-30 00:00:52 -0400

    Update prometheus.js

--- prometheus.js
+++ prometheus.js
@@ -293,6 +293,8 @@ client.on('message', message => {
 		message.reply("some von drama? I'm listening!");
 	} else if(message.content.toLowerCase().indexOf("auxice") != -1) {
 		message.reply("do it @Von#6488 !!");
+	} else if(message.content.toLowerCase().indexOf("tell us more stewy") != -1) {
+		message.reply("yeah we wanna know more mr. Bonkers");
 	}
 });
 
